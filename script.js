21af8e2c81d312e7267d9bf069a5947f93629ca8
FelipeRebula <rebulafelipe@gmail.com>
2026-01-20 14:06:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("bc");
+  const adm = document.getElementById("adm");
+
+  window.verificarLogin = function () {
+    if (
+      document.getElementById("input-u").value === "Marcus Hewit" &&
+      document.getElementById("input-s").value === "27092005"
+    ) {
+      document.getElementById("adm").style.visibility = "visible";
+      localStorage.setItem("logged", "1");
+    } else {
+      alert("Usuário ou senha incorretos");
+    }
+  };
+  if (localStorage.getItem("logged") === "1") {
+    document.getElementById("adm").style.visibility = "visible";
+  } else {
+    document.getElementById("adm").style.visibility = "hidden";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const titles = document.querySelectorAll(".doc-title");
+  const descriptions = document.querySelectorAll(".doc-des");
+
+  titles.forEach((title) => {
+    title.addEventListener("click", (e) => {
+      console.log("clicado");
+      e.preventDefault();
+
+      const target = title.dataset.target;
+
+      descriptions.forEach((desc) => {
+        desc.classList.remove("active");
+      });
+
+      document.getElementById(`doc-${target}`)?.classList.add("active");
+    });
+  });
+});
